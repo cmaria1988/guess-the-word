9ee665028b518012c769d3b77d59f2bb2098723e
Maria <claudmaria1988@gmail.com>
2023-11-12 20:05:09 +0800

    change in javascript

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const buttonPlayAgain = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLettersArray = [];
 
 const placeholder = function(word){
     const placeholderLetters = [];
@@ -23,6 +24,34 @@ placeholder(word);
 buttonGuess.addEventListener("click", function(e){
     e.preventDefault();
     const guess = letterInput.value;
-    console.log(guess);
+    if(validateInput(guess) != null){
+        makeGuess(guess);
+    }
+    console.log(guessedLettersArray);
     letterInput.value = "";
 });
+
+const validateInput = function(letter){
+    const acceptedLetter = /[a-zA-Z]/;
+    if(letter.length===0){
+        message.innerText = "Please enter a letter";
+    } else if(letter.length>1){
+        message.innerText = "Please enter a single letter";
+    }else if (!letter.match(acceptedLetter)){
+        message.innerText = "Please enter a letter from A-Z";
+    }else{
+        return letter;
+    };
+}
+
+
+const makeGuess = function(letters){
+    console.log("masuk sini");
+    letters = letters.toUpperCase();
+    if (!guessedLettersArray.includes(letters)){
+        guessedLettersArray.push(letters);
+        return letters;
+    }else{
+        message.innerText = "You already guessed that letter. Please try again!";
+    }
+}
